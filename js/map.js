e2a776ba0421ad1370eda25f0d9a5d387b357da2
vjandasek <73552431+vjandasek@users.noreply.github.com>
2021-10-10 22:05:50 +0200

    Map, colours, font, first template modification

---
+++ js/map.js
@@ -0,0 +1,12 @@
+var center = SMap.Coords.fromWGS84(18.4928969, 49.7533989);
+var m = new SMap(JAK.gel("m"), center, 13);
+m.addDefaultLayer(SMap.DEF_BASE).enable();
+m.addDefaultControls();
+
+var layer = new SMap.Layer.Marker();
+m.addLayer(layer);
+layer.enable();
+
+var options = {};
+var marker = new SMap.Marker(center, "myMarker", options);
+layer.addMarker(marker);
